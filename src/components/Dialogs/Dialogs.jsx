4879d482a8c8197fae355696065c2e154f471b90
Vertigo <vertigomaill@gmail.com>
2019-11-25 23:58:10 +0300

    Add Dialogs section

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import styles from "./Dialogs.module.css";
 
 const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={styles.dialogs}>
+      <div>
+        <div className={styles.userName}>Pasha</div>
+        <div className={styles.userName}>Dasha</div>
+        <div className={styles.userName}>Sasha</div>
+        <div className={styles.userName}>Masha</div>
+      </div>
+      <div>
+        <div className={styles.userMessages}>Hey Dude!</div>
+        <div className={styles.userMessages}>Wazzaup!</div>
+        <div className={styles.userMessages}>Link me!</div>
+        <div className={styles.userMessages}>Wrong chat!</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
