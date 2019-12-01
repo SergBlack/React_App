24b8778569d8ff806e4d64b9d1052811df78a88c
Vertigo <vertigomaill@gmail.com>
2019-12-01 23:41:14 +0300

    state.js, index.js refactoring

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,86 +1,77 @@
-let rerenderEntireTree = () => {
-  console.log("state is changed");
-};
+let store = {
+  _state: {
+    dialogsPage: {
+      messagesData: [
+        { id: 1, message: "Hey Dude!" },
+        { id: 2, message: "Wazzaup!" },
+        { id: 3, message: "Link me!" },
+        { id: 4, message: "Wrong chat!" }
+      ],
+      dialogsData: [
+        { id: 1, name: "Pasha" },
+        { id: 2, name: "Dasha" },
+        { id: 3, name: "Sasha" },
+        { id: 4, name: "Masha" }
+      ],
+      newMessageText: ""
+    },
 
-let state = {
-  dialogsPage: {
-    messagesData: [
-      { id: 1, message: "Hey Dude!" },
-      { id: 2, message: "Wazzaup!" },
-      { id: 3, message: "Link me!" },
-      { id: 4, message: "Wrong chat!" }
-    ],
-    dialogsData: [
-      { id: 1, name: "Pasha" },
-      { id: 2, name: "Dasha" },
-      { id: 3, name: "Sasha" },
-      { id: 4, name: "Masha" }
-    ],
-    newMessageText: ""
+    profilePage: {
+      postsData: [
+        { id: 1, message: "Hey", likesCount: "0" },
+        { id: 2, message: "Lets go", likesCount: "10" },
+        { id: 3, message: "Busy now", likesCount: "20" }
+      ],
+      newPostText: ""
+    },
+
+    sidebarUserOnline: {
+      user: [
+        { name: "Lenin", age: 33, sex: "male" },
+        { name: "Lara", age: 23, sex: "female" },
+        { name: "Pushkin", age: 66, sex: "male" },
+        { name: "show max 3 user online" },
+        { name: "show max 3 user online" }
+      ]
+    }
   },
-
-  profilePage: {
-    postsData: [
-      { id: 1, message: "Hey", likesCount: "0" },
-      { id: 2, message: "Lets go", likesCount: "10" },
-      { id: 3, message: "Busy now", likesCount: "20" }
-    ],
-    newPostText: ""
+  getState() {
+    return this._state;
   },
-
-  sidebarUserOnline: {
-    user: [
-      { name: "Lenin", age: 33, sex: "male" },
-      { name: "Lara", age: 23, sex: "female" },
-      { name: "Pushkin", age: 66, sex: "male" },
-      { name: "show max 3 user online" },
-      { name: "show max 3 user online" }
-    ]
+  _callSubscriber() {
+    console.log("state is changed");
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: "0"
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  addMessage() {
+    let newMessage = {
+      id: 5,
+      message: this._state.dialogsPage.newMessageText
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._state.dialogsPage.newMessageText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newText) {
+    this._state.dialogsPage.newMessageText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   }
 };
 
-const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: "0"
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-const updateNewPostText = newText => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-//Функция доб. сообщения
-const addMessage = () => {
-  let newMessage = {
-    id: 5,
-    message: state.dialogsPage.newMessageText
-  };
-  state.dialogsPage.messagesData.push(newMessage);
-  state.dialogsPage.newMessageText = "";
-  rerenderEntireTree(state);
-};
-
-const updateNewMessageText = newText => {
-  state.dialogsPage.newMessageText = newText;
-  rerenderEntireTree(state);
-};
-
-//observer (publisher-subscriber)
-const subscribe = observer => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
-export {
-  addPost,
-  updateNewPostText,
-  addMessage,
-  updateNewMessageText,
-  subscribe
-};
+export default store;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -3,29 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import state from "./Redux/state";
-import {
-  addPost,
-  updateNewPostText,
-  updateNewMessageText,
-  addMessage,
-  subscribe
-} from "./Redux/state";
+import store from "./Redux/state";
 
 let rerenderEntireTree = state => {
   ReactDOM.render(
     <App
       state={state}
-      addPost={addPost}
-      updateNewPostText={updateNewPostText}
-      addMessage={addMessage}
-      updateNewMessageText={updateNewMessageText}
+      addPost={store.addPost.bind(store)}
+      updateNewPostText={store.updateNewPostText.bind(store)}
+      addMessage={store.addMessage.bind(store)}
+      updateNewMessageText={store.updateNewMessageText.bind(store)}
     />,
     document.getElementById("root")
   );
 };
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
