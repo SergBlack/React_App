4fd4fbd9060c2ecdaa22e9ebf2d5362fc2a7b9ac
Vertigo <vertigomaill@gmail.com>
2019-12-08 20:33:25 +0300

    Add Axios

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -1,83 +1,82 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET-USERS";
-const MORE_USERS = "MORE-USERS";
 
 let initialState = {
   users: [
-    {
-      id: 1,
-      photoUrl:
-        "https://bumper-stickers.ru/31914-thickbox_default/gubka-bob.jpg",
-      follow: false,
-      firstname: "Pavel",
-      secondname: "Ivanov",
-      status: "I am here",
-      location: { city: "Moscow", country: "Russia" }
-    },
-    {
-      id: 2,
-      photoUrl: "https://slovnet.ru/wp-content/uploads/2018/11/2-2.jpeg",
-      follow: true,
-      firstname: "Sasha",
-      secondname: "Loparov",
-      status: "I am here too",
-      location: { city: "St.Peter", country: "Russia" }
-    },
-    {
-      id: 3,
-      photoUrl: "",
-      follow: false,
-      firstname: "Gleb",
-      secondname: "Kilov",
-      status: "Like you",
-      location: { city: "Warsaw", country: "Poland" }
-    },
-    {
-      id: 4,
-      photoUrl: "",
-      follow: false,
-      firstname: "Kostya",
-      secondname: "Petuhov",
-      status: "Relaxing",
-      location: { city: "Minsk", country: "Belarussia" }
-    },
-    {
-      id: 5,
-      photoUrl: "",
-      follow: false,
-      firstname: "Vova",
-      secondname: "Sobakin",
-      status: "Working",
-      location: { city: "Orel", country: "Russia" }
-    },
-    {
-      id: 6,
-      photoUrl: "",
-      follow: false,
-      firstname: "Petya",
-      secondname: "Koshkin",
-      status: "Sleeping",
-      location: { city: "Omsk", country: "Russia" }
-    },
-    {
-      id: 7,
-      photoUrl: "",
-      follow: false,
-      firstname: "Masha",
-      secondname: "Salova",
-      status: "Watching TV",
-      location: { city: "Podolsk", country: "Russia" }
-    },
-    {
-      id: 8,
-      photoUrl: "",
-      follow: false,
-      firstname: "Lera",
-      secondname: "Petrova",
-      status: "Listening music",
-      location: { city: "Himki", country: "Russia" }
-    }
+    //   {
+    //     id: 1,
+    //     photoUrl:
+    //       "https://bumper-stickers.ru/31914-thickbox_default/gubka-bob.jpg",
+    //     follow: false,
+    //     firstname: "Pavel",
+    //     secondname: "Ivanov",
+    //     status: "I am here",
+    //     location: { city: "Moscow", country: "Russia" }
+    //   },
+    //   {
+    //     id: 2,
+    //     photoUrl: "https://slovnet.ru/wp-content/uploads/2018/11/2-2.jpeg",
+    //     follow: true,
+    //     firstname: "Sasha",
+    //     secondname: "Loparov",
+    //     status: "I am here too",
+    //     location: { city: "St.Peter", country: "Russia" }
+    //   },
+    //   {
+    //     id: 3,
+    //     photoUrl: "",
+    //     follow: false,
+    //     firstname: "Gleb",
+    //     secondname: "Kilov",
+    //     status: "Like you",
+    //     location: { city: "Warsaw", country: "Poland" }
+    //   },
+    //   {
+    //     id: 4,
+    //     photoUrl: "",
+    //     follow: false,
+    //     firstname: "Kostya",
+    //     secondname: "Petuhov",
+    //     status: "Relaxing",
+    //     location: { city: "Minsk", country: "Belarussia" }
+    //   },
+    //   {
+    //     id: 5,
+    //     photoUrl: "",
+    //     follow: false,
+    //     firstname: "Vova",
+    //     secondname: "Sobakin",
+    //     status: "Working",
+    //     location: { city: "Orel", country: "Russia" }
+    //   },
+    //   {
+    //     id: 6,
+    //     photoUrl: "",
+    //     follow: false,
+    //     firstname: "Petya",
+    //     secondname: "Koshkin",
+    //     status: "Sleeping",
+    //     location: { city: "Omsk", country: "Russia" }
+    //   },
+    //   {
+    //     id: 7,
+    //     photoUrl: "",
+    //     follow: false,
+    //     firstname: "Masha",
+    //     secondname: "Salova",
+    //     status: "Watching TV",
+    //     location: { city: "Podolsk", country: "Russia" }
+    //   },
+    //   {
+    //     id: 8,
+    //     photoUrl: "",
+    //     follow: false,
+    //     firstname: "Lera",
+    //     secondname: "Petrova",
+    //     status: "Listening music",
+    //     location: { city: "Himki", country: "Russia" }
+    //   }
   ]
 };
 const usersReducer = (state = initialState, action) => {
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,53 +1,61 @@
 import React from "react";
 import styles from "./Users.module.css";
+import * as axios from "axios";
+import logo from "./../../images/logo.png";
 
-const Users = props => {
-  return (
-    <div>
-      {props.users.map(u => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img src={u.photoUrl} alt="ava" className={styles.avatar} />
-            </div>
-            <div>
-              {u.follow ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
+class Users extends React.Component {
+  componentDidMount() {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.users.map(u => (
+          <div key={u.id}>
             <span>
               <div>
-                {u.firstname} {u.secondname}
+                <img src={logo} alt="ava" className={styles.avatar} />
               </div>
-              <div>Status: "{u.status}"</div>
+              <div>
+                {u.follow ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
             </span>
             <span>
-              <div>
-                {u.location.city}, {u.location.country}
-              </div>
-              <div></div>
+              <span>
+                <div>{u.name}</div>
+                <div>Status: "{u.status}"</div>
+              </span>
+              <span>
+                <div>City</div>
+                <div></div>
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
