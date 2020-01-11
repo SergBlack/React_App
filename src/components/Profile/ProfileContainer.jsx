9d6afb671482b6aa73d3465fbd3a9c359eadb393
Vertigo <vertigomaill@gmail.com>
2020-01-11 12:15:36 +0300

    Add withRouter to ProfileContainer

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,17 +3,23 @@ import Profile from "./Profile";
 import { connect } from "react-redux";
 import * as axios from "axios";
 import { setUserProfile } from "./../../Redux/profile-reducer";
+import { withRouter } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/1542`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then(response => {
         this.props.setUserProfile(response.data);
       });
   }
 
   render() {
+    // debugger;
     return (
       <div>
         <Profile {...this.props} />
@@ -27,5 +33,7 @@ let mapStateToProps = state => {
     profile: state.profilePage.profile
   };
 };
-
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+let withUrlProfileContainer = withRouter(ProfileContainer);
+export default connect(mapStateToProps, { setUserProfile })(
+  withUrlProfileContainer
+);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -37,7 +37,7 @@ const ProfileInfo = props => {
         <div>
           Photos:{" "}
           <div>
-            <img src={props.profile.photos.large} alt="User photo" />
+            <img src={props.profile.photos.large} alt="avatar" />
           </div>
         </div>
       </div>
