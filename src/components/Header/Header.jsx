b13776698bdd0d53eed489841ee39db05ecc7fc9
Vertigo <vertigomaill@gmail.com>
2020-01-12 14:02:15 +0300

    Add Auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import logo from "../../images/logo.png";
 import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = props => {
   return (
     <header className={styles.header}>
       <h1>
         <img className={styles.img} src={logo} alt="LOGO" />
         Fakebook
       </h1>
+
+      <div className={styles.authLogin}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
     </header>
   );
 };
